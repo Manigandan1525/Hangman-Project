8beab8f1e9efe926a8ed1c5e851b04f78878799f
Manigandan1525 <32451747+Manigandan1525@users.noreply.github.com>
2018-02-15 20:17:38 +0530

    Add files via upload

---
+++ Demo Hangman/client.cpp
@@ -0,0 +1,37 @@
+#include "client.h"
+#include <winsock2.h>
+WSADATA WSAData;			//declaring the variable to connect server
+SOCKET server;
+SOCKADDR_IN addr;
+client::client()
+{
+}
+client::~client()
+{
+}
+string client::receive_details()
+{
+		char buffer[1024];
+		recv(server, buffer, sizeof(buffer), 0);			//receive data from server
+		string value(buffer);
+		return value;		//return string value
+}
+
+void client::send_request(string gamedetails)
+{
+	char tab2[1024];
+	strcpy(tab2, gamedetails.c_str());				//perform string copy operation
+	send(server, tab2, sizeof(tab2), 0);			//send data to server
+	return;
+}
+
+void client::connection()
+{
+
+	WSAStartup(MAKEWORD(2, 0), &WSAData);
+	server = socket(AF_INET, SOCK_STREAM, 0);
+	addr.sin_addr.s_addr = inet_addr("172.29.124.236"); 
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(138);
+	connect(server, (SOCKADDR *)&addr, sizeof(addr));		//connect to server
+}
